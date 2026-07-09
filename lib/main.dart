80a386c46fcaa85390a1e9553d9d27bfd27681ad
Omar Ashraf <omarbondok62@gmail.com>
2026-07-09 03:58:26 +0300

    Revert "added localization to the app"

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:fruit_hub/generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      locale: const Locale('en'),
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-
       theme: ThemeData(
         // This is the theme of your application.
         //
